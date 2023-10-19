5e085c7efd4d8d46a5286d07d962a363b1fe0bec
Adriano Araujo Valumin <adriano.valumin@outlook.com>
2023-10-18 21:29:14 -0300

    Part 1
    finished with some examples were given in this code.

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Adriano Valumin on 10/18/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
